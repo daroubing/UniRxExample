cb8bfbd6e4792dc74684df6dc19e573ab6ce9014
zhaobinkun <zhaobinkun@cmcm.com>
2017-12-20 21:15:46 +0800

    修改LoginViewModel

--- Assets/Example/ViewModel/Editor/LoginViewModelTests.cs
+++ Assets/Example/ViewModel/Editor/LoginViewModelTests.cs
@@ -1,20 +1,30 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using UniRx;
 
 
-namespace Example.ViewModel.Editor {
-    public class LoginViewModelTests {
-
+namespace Example.ViewModel.Editor
+{
+    public class LoginViewModelTests
+    {
         [Test]
-        public void InputName_Pass()
+        public void InputPassword_Success()
         {
             var viewModel = new LoginViewModel();
 
-            Observable.Return("ABC").Subscribe(x => viewModel.InputPasswordCommand.Execute(x));
+            Observable.Return(LoginViewModel.PASSWORD).Subscribe(x => viewModel.InputPasswordCommand.Execute(x));
 
             Assert.AreEqual(LoginViewModel.SUCCESS_TIPS, viewModel.Tips.Value);
         }
 
 
+        [Test]
+        public void InputPassword_Error()
+        {
+            var viewModel = new LoginViewModel();
+
+            Observable.Return("ErrorPassword").Subscribe(x => viewModel.InputPasswordCommand.Execute(x));
+
+            Assert.AreEqual(LoginViewModel.ERROR_TIPS, viewModel.Tips.Value);
+        }
     }
 }
--- Assets/Example/ViewModel/LoginViewModel.cs
+++ Assets/Example/ViewModel/LoginViewModel.cs
@@ -3,38 +3,40 @@ using UniRx;
 
 namespace Example.ViewModel
 {
-public class LoginViewModel
-{
-    /// <summary>
-    /// 正确的密码
-    /// </summary>
-    public const string PASSWORD = "ABC";
-
-    /// <summary>
-    /// 提示初始化文字
-    /// </summary>
-    public const string INPUT_TIPS = "请输入密码";
-
-    //正确提示与错误提示
-    public const string SUCCESS_TIPS = "密码正确";
-    public const string ERROR_TIPS = "密码错误,请重新输入";
-
-    public ReadOnlyReactiveProperty<string> Tips { get; private set; }
-
-    public ReactiveCommand<string> InputPasswordCommand { get; private set; }
-
-
-    public LoginViewModel()
+    public class LoginViewModel
     {
-        InputPasswordCommand = new ReactiveCommand<string>();
-        //绑定输入命令,检查密码,转换为提示内容
-        Tips = InputPasswordCommand.Select(CheckName).StartWith(INPUT_TIPS).ToReadOnlyReactiveProperty();
-    }
+        /// <summary>
+        /// 正确的密码
+        /// </summary>
+        public const string PASSWORD = "ABC";
+
+        /// <summary>
+        /// 提示初始化文字
+        /// </summary>
+        public const string INPUT_TIPS = "请输入密码";
+
+        //正确提示与错误提示
+        public const string SUCCESS_TIPS = "密码正确";
+
+        public const string ERROR_TIPS = "密码错误,请重新输入";
+
+        public ReadOnlyReactiveProperty<string> Tips { get; private set; }
+
+        public ReactiveCommand<string> InputPasswordCommand { get; private set; }
 
 
-    private string CheckName(string name)
-    {
-        return name.Equals(PASSWORD) ? SUCCESS_TIPS : ERROR_TIPS;
+        public LoginViewModel()
+        {
+            InputPasswordCommand = new ReactiveCommand<string>();
+
+            //绑定输入命令,检查密码,转换为提示内容
+            Tips = InputPasswordCommand.Select(CheckName).StartWith(INPUT_TIPS).ToReadOnlyReactiveProperty();
+        }
+
+
+        private string CheckName(string name)
+        {
+            return name.Equals(PASSWORD) ? SUCCESS_TIPS : ERROR_TIPS;
+        }
     }
-}
 }
